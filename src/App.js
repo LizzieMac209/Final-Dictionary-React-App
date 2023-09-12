f9957954b949cd208ebe8b25dbb9776907ff37d7
LizzieMac209 <Emacfarlane05@gmail.com>
2023-09-11 21:55:29 -0700

    Had to start from scratch and reupload the code from previous dictionary app code- finally was able to launch netlify correctly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <h1>
-        Hello
-        </h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href=" https://github.com/LizzieMac209/dictionary-react-app-project "
+              target="_blank"
+            >
+              Lizzie Macfarlane
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
